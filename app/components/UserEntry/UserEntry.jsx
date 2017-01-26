c12455f3fd838c94d29fb10b3ee860f7517021a0
Blake Hagen <blakehagen@gmail.com>
2017-01-25 21:48:38 -0700

    entry form toggle, styling updates

--- app/components/UserEntry/UserEntry.jsx
+++ app/components/UserEntry/UserEntry.jsx
@@ -4,7 +4,41 @@ import ActionButton from './ActionButton';
 import styles from './userEntry.scss';
 
 export default class UserEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state       = {type: 'login'};
+    this.toggleEntry = this.toggleEntry.bind(this);
+
+  }
+
   render() {
+    let entryForm;
+    let switchFormText;
+
+    if (this.state.type === 'login') {
+      entryForm          = (
+        <div className={styles.inputContainer}>
+          <InputField type="text" placeholder="Email"/>
+          <InputField style={{marginTop: '8px'}} type="password" placeholder="Password"/>
+          <ActionButton buttonAction="Login"/>
+        </div>
+      );
+      switchFormText = 'Register';
+    } else {
+      entryForm          = (
+        <div className={styles.inputContainer}>
+          <InputField type="text" placeholder="First Name"/>
+          <InputField style={{marginTop: '8px'}} type="text" placeholder="Last Name"/>
+          <InputField style={{marginTop: '8px'}} type="text" placeholder="Email"/>
+          <InputField style={{marginTop: '8px'}} type="password" placeholder="Password"/>
+          <InputField style={{marginTop: '8px'}} type="password" placeholder="Confirm Password"/>
+          <ActionButton buttonAction="Register"/>
+        </div>
+      );
+      switchFormText = 'Login';
+
+    }
+
     return (
       <div className={styles.mainContainer}>
         <div className={styles.formContainer}>
@@ -12,16 +46,24 @@ export default class UserEntry extends React.Component {
             <div className={styles.snailIcon}/>
             <h1 className={styles.title}>SnailBox</h1>
           </div>
-          <div className={styles.inputContainer}>
-            <InputField type="text" placeholder="Email"/>
-            <InputField style={{marginTop: '8px'}} type="password" placeholder="Password"/>
-            <ActionButton buttonAction="Login"/>
-          </div>
-          <div className={styles.actionToggle}>
-            <p>Sign Up</p>
+          {entryForm}
+          <div className={styles.actionToggle} onClick={this.toggleEntry}>
+            <p>{switchFormText}</p>
           </div>
         </div>
       </div>
     )
+  }
+
+  toggleEntry() {
+    if (this.state.type === 'login') {
+      this.setState({
+        type: 'register'
+      });
+    } else {
+      this.setState({
+        type: 'login'
+      });
+    }
   }
 }
